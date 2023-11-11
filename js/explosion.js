81869feecba261b663382a6bf5b28ac192bf7bb7
cyclic-group <cshen-1123@proton.me>
2023-11-12 02:07:18 +0800

    Site updated: 2023-11-12 02:07:16

---
+++ js/explosion.js
@@ -0,0 +1,16 @@
+document.querySelector(".page-main-content-middle").addEventListener(
+    "mousedown",
+    () => {
+        alert("💥 💥💥 💥💥💥💥");
+        location.href = "https://wwwwwwwww.jodi.org/";
+        return false; 
+    }
+)
+document.querySelector(".page-main-content-middle").addEventListener(
+    "mouseup",
+    () => {
+        alert("💥 💥💥 💥💥💥💥");
+        location.href = "https://wwwwwwwww.jodi.org/";
+        return false; 
+    }
+)
