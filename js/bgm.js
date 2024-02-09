812d11c627b9859d83e7df014b947f8d9f5f10e0
cyclic-group <cshen-1123@proton.me>
2024-02-10 00:08:45 +0800

    Site updated: 2024-02-10 00:08:45

--- js/bgm.js
+++ js/bgm.js
@@ -1,4 +1,4 @@
-// let current_bgm = undefined;
+let current_bgm = 0;
 const bgm = document.createElement("audio");
 bgm.id = "bgm";
 bgm.src = undefined;
@@ -14,21 +14,17 @@ window.onload = function() {
     widget.style.cursor = "url('/images/clef.png') 20 20, auto";
     widget.addEventListener("mousedown", () => {
         // place music sources to this list
-        // const audio_list = ["https://vgmsite.com/soundtracks/cult-of-the-lamb-original-soundtrack-2022/upclzsolxj/01.%20Praise%20the%20Lamb.mp3", "https://vgmsite.com/soundtracks/cult-of-the-lamb-original-soundtrack-2022/lbccbknyve/05.%20Faith%20Up.mp3", "https://vgmsite.com/soundtracks/cult-of-the-lamb-original-soundtrack-2022/biuzbjcrtj/07.%20Saviour.mp3", 
-        //"https://vgmsite.com/soundtracks/cult-of-the-lamb-original-soundtrack-2022/rxoopwecuw/08.%20Darkwood.mp3", "https://vgmsite.com/soundtracks/cult-of-the-lamb-original-soundtrack-2022/mwplkyfcej/10.%20Leshy.mp3", "https://vgmsite.com/soundtracks/cult-of-the-lamb-original-soundtrack-2022/pyrxysussx/11.%20Knucklebones.mp3",
-        //"https://vgmsite.com/soundtracks/cult-of-the-lamb-original-soundtrack-2022/bmshkiptnt/16.%20Heket.mp3", "https://vgmsite.com/soundtracks/cult-of-the-lamb-original-soundtrack-2022/shfpubazuq/26.%20Shamura.mp3", "https://vgmsite.com/soundtracks/cult-of-the-lamb-original-soundtrack-2022/cjseknykhh/35.%20The%20One%20Who%20Waits.mp3"];
-        // const audio_list = [
-        //     "/audios/megalovania.mp3",
-        //     "/audios/the-one-who-waits.mp3",
-        //     "/audios/faith-up.mp3",
-        //     "/audios/rennala-queen-of-the-full-moon.mp3"
-        // ];
-        // do { 
-        //     next_bgm = Math.floor(Math.random() * audio_list.length);
-        // } while (next_bgm === current_bgm);
-        // current_bgm = next_bgm;
-        // bgm.src = audio_list[current_bgm];
-        bgm.src = "/audios/rennala-queen-of-the-full-moon.mp3";
+        const audio_list = [
+            "/audios/rennala-queen-of-the-full-moon.mp3",
+            "/audios/megalovania.mp3",
+            // "/audios/the-one-who-waits.mp3",
+            "/audios/faith-up.mp3"
+        ];
+        do { 
+            next_bgm = Math.floor(Math.random() * audio_list.length);
+        } while (next_bgm === current_bgm);
+        current_bgm = next_bgm;
+        bgm.src = audio_list[current_bgm];
         bgm.play();
         return false;
     });
